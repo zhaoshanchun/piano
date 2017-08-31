e6387977eda699e5fe0cfd6a44f018ff5f6caf0f
zhaoshanchun <zhaoshanchun@foxmail.com>
2017-09-01 01:16:15 +0800

    Add VideoDetailViewController

---
+++ Project/VideoDetail/VideoDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  VideoDetailViewController.h
+//  gangqinjiaocheng
+//
+//  Created by zhaosc on 17/8/30.
+//  Copyright © 2017年 kun. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface VideoDetailViewController : BaseViewController
+
+@end
---
+++ Project/VideoDetail/VideoDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  VideoDetailViewController.m
+//  gangqinjiaocheng
+//
+//  Created by zhaosc on 17/8/30.
+//  Copyright © 2017年 kun. All rights reserved.
+//
+
+#import "VideoDetailViewController.h"
+
+@interface VideoDetailViewController ()
+
+@end
+
+@implementation VideoDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
